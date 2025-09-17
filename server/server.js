4904214e0be3588b9cb30cf3c6e7945996c902e7
beridzezura14 <zuraberidze12@gmail.com>
2025-09-17 12:59:11 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import heroRoutes from "./routes/heroRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import adminRoutes from "./routes/admin.js";
 
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// __dirname fix (ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
@@ -23,13 +28,11 @@ app.use("/api/upload", uploadRoutes);
 app.use("/api/hero", heroRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 });
 
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-

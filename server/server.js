85fd2bf1d9c80f795b616cb8d8706d50308073af
beridzezura14 <zuraberidze12@gmail.com>
2025-09-17 12:53:08 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 import connectDB from "./config/db.js";
 import heroRoutes from "./routes/heroRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import adminRoutes from "./routes/admin.js";
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // DB connect
@@ -21,6 +22,13 @@ connectDB();
 app.use("/api/upload", uploadRoutes);
 app.use("/api/hero", heroRoutes);
 app.use("/api/admin", adminRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
